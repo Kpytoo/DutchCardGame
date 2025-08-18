f84b30c978269d71d61799c328b260f404d346d4
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-08-18 13:25:32 -0400

    Changed "game rules" to "settings"

--- game_tryout.js
+++ game_tryout.js
@@ -156,15 +156,15 @@ button_dutch.addEventListener("click", (e) =>{
     }
 });
 
-button_game_rules.addEventListener("click", (e) => {
-    container_game_rules_information.style.display = "flex";
-    container_game_rules_information.style.animationName = "button_pop_in";
+button_settings.addEventListener("click", (e) => {
+    container_game_settings.style.display = "flex";
+    container_game_settings.style.animationName = "button_pop_in";
 });
 
 button_exit_rules.addEventListener("click", (e) => {
-    container_game_rules_information.style.animationName = "button_pop_out";
-    container_game_rules_information.addEventListener("animationend", (e) =>{
-        container_game_rules_information.style.display = "none";   
+    container_game_settings.style.animationName = "button_pop_out";
+    container_game_settings.addEventListener("animationend", (e) =>{
+        container_game_settings.style.display = "none";   
     }, {once: true});
 });
 
